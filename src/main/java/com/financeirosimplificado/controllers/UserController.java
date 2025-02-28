79e0abae38217ba12e1930bd621430b60ede37cd
Ericson Breno <ericsonbreno78@gmail.com>
2025-02-28 13:44:22 -0300

    feat: user controller

--- src/main/java/com/financeirosimplificado/controllers/UserController.java
+++ src/main/java/com/financeirosimplificado/controllers/UserController.java
@@ -1,16 +1,21 @@
 package com.financeirosimplificado.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.financeirosimplificado.domain.user.User;
 import com.financeirosimplificado.dto.UserDTO;
 import com.financeirosimplificado.services.UserService;
 
-@RestController("/user")
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
@@ -21,6 +26,13 @@ public class UserController {
         User newUser = this.userService.createUser(user);
 
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> listUsers() {
+        List<User> users = this.userService.getAllUsers();
+
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
 }
