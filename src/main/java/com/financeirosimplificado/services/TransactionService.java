6967a6bc3167e5b8ad4c50649ed303886975fce0
Ericson Breno <ericsonbreno78@gmail.com>
2025-02-28 11:54:34 -0300

    feat: Transaction service

--- src/main/java/com/financeirosimplificado/services/TransactionService.java
+++ src/main/java/com/financeirosimplificado/services/TransactionService.java
@@ -1,10 +1,17 @@
 package com.financeirosimplificado.services;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.financeirosimplificado.domain.transaction.Transaction;
 import com.financeirosimplificado.domain.user.User;
 import com.financeirosimplificado.dto.TransactionDTO;
 import com.financeirosimplificado.repositories.TransactionRepository;
@@ -17,12 +24,42 @@ public class TransactionService {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     public void createTransaction (TransactionDTO transaction) throws Exception{
 
         User payer = this.userService.findUserById(transaction.payerId());
         User payee = this.userService.findUserById(transaction.payeeId());
         
         this.userService.validateTransaction(payer, transaction.amount());
+
+        boolean authorized = this.authorizeTransaction(payer, transaction.amount()); 
+
+        if ( !authorized ) {
+            throw new Exception("Transaction not authorized");
+        }
+
+        Transaction newTransaction = new Transaction();
+
+        newTransaction.setPayer(payer);
+        newTransaction.setPayee(payee);
+        newTransaction.setAmount(transaction.amount());
+        newTransaction.setTimestamp(LocalDateTime.now());
+
+    }
+
+    public boolean authorizeTransaction(User payer, BigDecimal amount) {
+
+        ResponseEntity<Map> authorizationResponse = this.restTemplate.getForEntity("https://util.devi.tools/api/v2/authorize", Map.class);
+
+        if (authorizationResponse.getStatusCode() == HttpStatus.OK) {
+
+            String message = (String) authorizationResponse.getBody().get("message").toString();
+
+            return "Autorizado".equals(message);
+
+        } else return false;
 
     }
 
