01e51dbc502111253f022780993d282c5e9983ff
Ericson Breno <ericsonbreno78@gmail.com>
2025-02-28 12:36:49 -0300

    bug: notification dto

--- src/main/java/com/financeirosimplificado/dto/TransactionDTO.java
+++ src/main/java/com/financeirosimplificado/dto/TransactionDTO.java
@@ -1,10 +1,9 @@
 package com.financeirosimplificado.dto;
 
+import java.math.BigDecimal;
+
 import com.financeirosimplificado.domain.user.User;
 
 public record TransactionDTO (Long payerId, Long payeeId, BigDecimal amount) {
-    User payer = this.userService.findUserById(payerId);
-    User payee = this.userService.findUserById(payeeId);
-    this.userService.validateTransaction(payer, amount);
-
+    
 }
